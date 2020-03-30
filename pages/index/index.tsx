4d2d66d01069d7a46b6ace5d01babe56c3b7c3de
joonajo <joona.joenpolvi@gmail.com>
2020-03-30 21:55:32 +0300

    Initial commit, set up io-ts & .babelrc
    Setting up the project. Trying to learn io-ts (...). Added .babelrc to fix styled-components className warning.

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import * as t from 'io-ts'
+import { ThrowReporter } from 'io-ts/lib/ThrowReporter'
 
 import { NextPage } from 'next'
 import styled from 'styled-components'
@@ -17,13 +19,36 @@ const Title = styled.h1`
     font-weight: normal;
 `
 
-const Text = styled.p`
-    
+const Text = styled.p`   
 `
 
+const Data = t.type({
+    date: t.string,
+    confirmed: t.number,        
+    deaths: t.number,
+    recovered: t.number
+})
+
+const ResponseData = t.type({
+    Finland: t.array(Data),
+    Sweden: t.array(Data),
+    Italy: t.array(Data),
+    Spain: t.array(Data),
+})
+
 const App: NextPage = (): JSX.Element => {
+    const [countries, setCountries] = React.useState(null)
+
     React.useEffect(() => {
-        document.title = "NEXT-REACT-TS"
+        document.title = "COVID-19 Trckr"
+
+        fetch("https://pomber.github.io/covid19/timeseries.json")
+            .then(response => response.json())
+            .then(data => {
+                const result = ResponseData.decode(data)
+                ThrowReporter.report(result)
+                setCountries(data)
+            }).catch(e => console.log(e))
     }, [])
 
     return (
